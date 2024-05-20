8f9cc0e93cb86b511366aa91a57b071803cb427e
Jake Walker <hi@jakew.me>
2024-05-20 21:05:21 +0100

    Add comments

--- src/lib.rs
+++ src/lib.rs
@@ -12,20 +12,25 @@ pub struct Game {
 }
 
 impl Game {
+    /// List all player indexes in the game
     pub fn players(&self) -> Vec<usize> {
         let mut players = self.team_a.clone();
         players.extend(self.team_b.clone());
         players
     }
 
+    /// List the pairs of player indexes in the game
     pub fn pairs(&self) -> [&Vec<usize>; 2] {
         [&self.team_a, &self.team_b]
     }
 }
 
 #[derive(Debug, PartialEq, Clone)]
+/// Strategy for generating games
 pub enum GenStrategy {
+    /// No shuffling and (probably) deterministic
     NORMAL = 0,
+    /// Players are shuffled before generating games
     SHUFFLED = 1
 }
 
@@ -36,11 +41,14 @@ impl Display for GenStrategy {
 }
 
 #[derive(Debug, Clone)]
+/// A session where multiple games are played with the same group of people
 pub struct Session {
     pub player_names: Vec<String>,
     pub games: Vec<Game>,
     pub gen_strategy: GenStrategy,
+    /// The number of courts for the session
     pub courts: usize,
+    /// The size of the team on one side of the court (i.e. 1 for singles, 2 for doubles)
     pub team_size: usize
 }
 
@@ -53,6 +61,9 @@ impl Session {
         self.games.push(g)
     }
 
+    /// Remove a player from the session by their name
+    ///
+    /// This requires the game history to be reset
     pub fn remove_player(&mut self, player: &str) {
         if let Some(player_index) = self.player_names.iter().position(|x| *x == player) {
             self.games = Vec::new();
@@ -60,6 +71,7 @@ impl Session {
         }
     }
 
+    /// Counts for the number of games each player has played
     pub fn player_game_counts(&self) -> HashMap<usize, usize> {
         let mut counts = HashMap::new();
 
@@ -70,6 +82,7 @@ impl Session {
         counts
     }
 
+    /// Counts for the number of games each pair has played
     pub fn pair_game_counts(&self) -> HashMap<&Vec<usize>, usize> {
         let mut counts = HashMap::new();
 
@@ -80,14 +93,16 @@ impl Session {
         counts
     }
 
+    /// Suggest the next game to be played
     pub fn next_game(&self) -> Option<Game> {
-        let games_to_exclude = self.games.len().rem(self.courts);
+        // calculate the remainder for games divided by courts to get the 'court number' for this game
+        let court_number = self.games.len().rem(self.courts);
 
-        // if there is more than 1 court, make sure the next game doesn't include the game for the previous court(s)
-        // unless this is the first of the courts, then don't exclude any players
         let to_exclude: Vec<usize> = {
-            if self.courts > 1 && games_to_exclude > 0 {
-                self.games.iter().rev().take(games_to_exclude).flat_map(|game| game.players()).collect_vec()
+            // if there is more than 1 court and it is not the first court
+            if self.courts > 1 && court_number > 0 {
+                // get the players from the first courts and make sure they are not included in the next game
+                self.games.iter().rev().take(court_number).flat_map(|game| game.players()).collect_vec()
             } else {
                 Vec::new()
             }
@@ -96,52 +111,73 @@ impl Session {
         self.generate_game(to_exclude)
     }
 
+    /// Generate a game given a list of players to exclude
     pub fn generate_game(&self, excluded_player_indexes: Vec<usize>) -> Option<Game> {
+        // get the number of player and pair counts
         let player_game_counts = self.player_game_counts();
         let pair_game_counts = self.pair_game_counts();
 
-        let mut player_ids = self.player_names.iter().enumerate().map(|(index, _)| index).collect_vec();
+        // get player indexes and remove those which are in the exclusion list
+        let mut player_ids = self.player_names.iter().enumerate()
+            .map(|(index, _)| index)
+            .filter(|i| !excluded_player_indexes.contains(i))
+            .collect_vec();
 
+        // optionally shuffle the player indexes to give games a more random feel
         if self.gen_strategy == GenStrategy::SHUFFLED {
             player_ids.shuffle(&mut thread_rng());
         }
 
-        let mut game_candidates = player_ids.into_iter().combinations(self.team_size).combinations(2).filter(|teams| {
-            let mut counts = HashSet::new();
-            let mut repeated = HashSet::new();
+        // calculate potential games for the list of player ids
+        let mut game_candidates = player_ids.into_iter()
+            // get team combinations (i.e. one side of a court)
+            .combinations(self.team_size)
+            // get game combinations (i.e. combinations of two teams)
+            .combinations(2)
+            // remove any games that aren't possible
+            .filter(|teams| {
+                let mut counts = HashSet::new();
+                let mut repeated = HashSet::new();
 
-            for team in teams {
-                for player in team {
-                    if excluded_player_indexes.contains(player) {
-                        return false;
-                    }
-
-                    let count = counts.insert(player);
-                    if !count {
-                        repeated.insert(*player);
+                for team in teams {
+                    for player in team {
+                        let count = counts.insert(player);
+                        // this player is used twice in the game
+                        if !count {
+                            repeated.insert(*player);
+                        }
                     }
                 }
-            }
 
-            repeated.len() == 0
-        }).sorted_by(|a, b| {
-            let a_game_count: usize = a.iter().flatten().map(|player| player_game_counts.get(player).unwrap_or_else(|| &0)).sum();
-            let b_game_count: usize = b.iter().flatten().map(|player| player_game_counts.get(player).unwrap_or_else(|| &0)).sum();
+                repeated.len() == 0
+            })
+            // sort games to find the best - prioritise games where players have played the least and where the pairs are new
+            .sorted_by(|a, b| {
+                // for both games, sum the number of games each player has played
+                let a_game_count: usize = a.iter().flatten().map(|player| player_game_counts.get(player).unwrap_or_else(|| &0)).sum();
+                let b_game_count: usize = b.iter().flatten().map(|player| player_game_counts.get(player).unwrap_or_else(|| &0)).sum();
 
-            let a_pair_counts: usize = a.iter().map(|pair| pair_game_counts.get(pair).unwrap_or_else(|| &0)).sum();
-            let b_pair_counts: usize = b.iter().map(|pair| pair_game_counts.get(pair).unwrap_or_else(|| &0)).sum();
+                // for both games, sum the number of games each team has played
+                let a_team_counts: usize = a.iter().map(|pair| pair_game_counts.get(pair).unwrap_or_else(|| &0)).sum();
+                let b_team_counts: usize = b.iter().map(|pair| pair_game_counts.get(pair).unwrap_or_else(|| &0)).sum();
 
-            let a_total = a_game_count + a_pair_counts;
-            let b_total = b_game_count + b_pair_counts;
+                // total the number of games each player and team has played
+                let a_total = a_game_count + a_team_counts;
+                let b_total = b_game_count + b_team_counts;
 
-            a_total.cmp(&b_total)
-        }).map(|teams| {
-            Game { team_a: teams[0].clone(), team_b: teams[1].clone() }
-        });
+                // sort the games so the least number of plays comes first
+                a_total.cmp(&b_total)
+            })
+            // convert the list into a game
+            .map(|teams| {
+                Game { team_a: teams[0].clone(), team_b: teams[1].clone() }
+            });
 
+        // return the first game (i.e. the best)
         game_candidates.next()
     }
 
+    /// Convert a game struct to a string with player names
     pub fn format_game(&self, game: &Game) -> String {
         let default: &String = &"?".to_string();
         game.pairs().map(|pair| pair.iter().map(|i| self.player_names.get(*i).unwrap_or_else(|| default)).join(" and ")).join(" vs. ")
