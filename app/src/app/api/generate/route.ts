79e53ebafc1bedfe89d89717ebe2c28529a814d3
Jake Walker <hi@jakew.me>
2024-06-30 20:34:57 +0100

    Add game generation REST API

---
+++ app/src/app/api/generate/route.ts
@@ -0,0 +1,45 @@
+import { z } from "zod";
+import * as generator from "generator";
+import { v4 as uuid } from "uuid";
+
+const teamSize = 2;
+
+const schema = z.object({
+  players: z.string().min(1).array(),
+  courtCount: z.number().min(1).max(100).finite().safe().int().default(1),
+  gameCount: z.number().min(1).max(100).finite().safe().int().default(20)
+}).refine((data) => data.players.length >= (teamSize * data.courtCount), { message: "There are not enough players defined." });
+
+export async function POST(request: Request) {
+  const { data, error, success } = schema.safeParse(await request.json());
+
+  if (!success) {
+    return Response.json({ errors: error.errors }, { status: 400 });
+  }
+
+  const session: generator.Session = {
+    matches: [],
+    players: data.players.map((name) => ({
+      id: uuid(),
+      name
+    }))
+  };
+
+  for (let i = 0; i < data.gameCount; i++) {
+    const nextGame = generator.nextGame(session, data.courtCount);
+
+    if (nextGame === null) {
+      return Response.json({ error: "Failed to generate game" }, { status: 500 });
+    }
+
+    session.matches.push(nextGame);
+  }
+
+  return Response.json({
+    matches: session.matches.map((match, i) => ({
+      ...match,
+      index: i,
+      formatted: generator.formatMatch(match)
+    }))
+  });
+}
